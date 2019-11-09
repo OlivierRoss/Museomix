5b60f4ff790cd3f621ec4b973db6ec672ccc85a6
Olivier Ross <olivier.ross@usherbrooke.ca>
2019-11-09 14:27:41 -0500

    video fonctionnel

--- animation.js
+++ animation.js
@@ -1,11 +1,24 @@
+var gauche = 0;
+
 function animer () {
-  document.getElementById("caroussel").className = "gauche";
+  vers_gauche(0);
   changer_temps();
 }
 
 function changer_temps () {
   document.getElementById("temps").innerHTML = new Date();
   setTimeout(changer_temps, 1000);
+}
+
+function vers_gauche (gauche) {
+  gauche -= 1;
+  var caroussel = document.getElementById("caroussel");
+  caroussel.style.left = gauche + "px";
+  if(gauche * -1 < caroussel.offsetWidth) {
+    setTimeout(() => {
+      vers_gauche(gauche);
+    }, 10);
+  }
 }
 
 window.onload = animer;
